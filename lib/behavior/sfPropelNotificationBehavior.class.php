ccf209630700996614592f5c1a0c7bbc0d7a4407
trivoallan <trivoallan@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-04-30 10:12:32 -0500

    [wiki:sfPropelNotificationPlugin] : typo
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelNotificationPlugin/trunk@8692 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/behavior/sfPropelNotificationBehavior.class.php
+++ lib/behavior/sfPropelNotificationBehavior.class.php
@@ -1,16 +1,16 @@
 <?php
 /*
  * This file is part of the sfPropelNotificationPlugin package.
- * 
+ *
  * (c) 2006-2007 Tristan Rivoallan <tristan@rivoallan.net>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
 /**
  * This behavior add automatic notification triggering on object modification.
- * 
+ *
  * @package     sfPropelNotificationPlugin
  * @subpackage  behavior
  * @author      Tristan Rivoallan <tristan@rivoallan.net>
@@ -23,7 +23,7 @@ class sfPropelNotificationBehavior
   /**
    * This hook to keep object "novelty" once it is saved. It also saves object's state before saving
    * it to enable later comparisons.
-   * 
+   *
    * @param    BaseObject    $object
    */
   public function presave(BaseObject $object)
@@ -32,24 +32,24 @@ class sfPropelNotificationBehavior
     {
       $object->setWasNew(true);
     }
-  
+
     // Save object state prior to saving
     $object->setBeforeSave($object);
   }
-  
+
   /**
    * Object novelty is lost after an update.
-   * 
+   *
    * @param    BaseObject    $object
    */
   public function postUpdate(BaseObject $object)
   {
     $object->setWasNew(false);
   }
-  
+
   /**
    * Triggers notification once the object has been successfuly saved to database.
-   * 
+   *
    * @param    BaseObject    $object
    */
   public function postSave(BaseObject $object)
@@ -68,7 +68,7 @@ class sfPropelNotificationBehavior
   /**
    * Returns true if object was new before being saved to database and false if save was just about
    * updating the object.
-   * 
+   *
    * @param    BaseObject    $object
    * @return   bool
    */
@@ -80,7 +80,7 @@ class sfPropelNotificationBehavior
   /**
    * Returns true if object was new before being saved to database and false if save was just about
    * updating the object.
-   * 
+   *
    * @param    BaseObject    $object
    * @return   bool
    */
@@ -91,7 +91,7 @@ class sfPropelNotificationBehavior
 
   /**
    * Sets object novelty status.
-   *  
+   *
    * @param    BaseObject    $object
    * @param    bool          $new
    */
@@ -103,7 +103,7 @@ class sfPropelNotificationBehavior
   /**
    * Saves object instance into "before_save" property. It can be called later (in notification
    * logic provider for instance) with getBeforeSave(). Useful for before / after comparisons.
-   * 
+   *
    * @param    BaseObject    $object
    */
   public function setBeforeSave(BaseObject $object)
@@ -113,12 +113,15 @@ class sfPropelNotificationBehavior
 
   /**
    * Returns object instance as it was when the preSave() hook was called.
-   * 
+   *
    * @return    BaseObject    $object
    */
-  public function getBeforeSave()
+  public function getBeforeSave(BaseObject $object)
   {
-    return $this->before_save;
+    if (isset($object->before_save))
+    {
+      return $object->before_save;
+    }
   }
 
 }
